f181ddceadf1789d569dd201ee0933c421d22e34
SqTione <sqtione@icloud.com>
2024-08-27 01:19:12 +0300

    Main Screen layout in process

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,24 @@ module.exports = {
         fontFamily: {
             lato: ['Lato', 'sans-serif'],
             nauryz: ['NauryzRedKeds', 'sans-serif'],
-        }
+        },
+        fontSize: {
+            'xs': '0.875rem',       //14px
+            'sm': '1rem',           // 16px
+            'base': '1.125rem',     // 18px
+            'xl': '1.25rem',        // 20px
+            '2xl': '1.5rem',        // 24px
+            '3xl': '2.25rem',       // 36px
+            '4xl': '3rem',          // 48px
+            '5xl': '4rem',          // 64px
+            '7xl': '6rem',          // 96px
+        },
+        extend: {
+            lineHeight: {
+                '18': '4rem',
+                '20': '6rem'
+            }
+        },
     },
     plugins: [],
 }
