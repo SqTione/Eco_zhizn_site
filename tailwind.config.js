df3fac084d8afa342a3b187d1f5387e97a8e2e66
SqTione <sqtione@icloud.com>
2024-08-29 12:54:01 +0300

    Created responsive main screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,18 @@ module.exports = {
             '3xl': '2.25rem',       // 36px
             '4xl': '3rem',          // 48px
             '5xl': '4rem',          // 64px
+            '6xl': '5rem',
             '7xl': '6rem',          // 96px
+        },
+        screens: {
+            '3xl': {'max': '1920px'},
+            '2xl': {'max': '1366px'},
+            'xl': {'max': '1280px'},
+            'lg': {'max': '1024px'},
+            'md': {'max': '768px'},
+            '3sm': {'max': '560px'},
+            '2sm': {'max': '450px'},
+            'sm': {'max': '390px'},
         },
         extend: {
             lineHeight: {
