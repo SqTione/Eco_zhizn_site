74ff27c91ba7b6ceb203378efe1afcea99e06ac9
SqTione <sqtione@icloud.com>
2024-09-08 00:30:01 +0300

    Added popup for Partners

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const overlay = document.getElementById("overlay");
+
     // Toggle sidebar handler
     const sidebarButton = document.querySelectorAll(".sidebar__button");
     const sidebar = document.querySelector("#sidebar");
@@ -8,6 +10,30 @@ document.addEventListener("DOMContentLoaded", () => {
             e.preventDefault();
             btn.classList.toggle("sidebar__button--active");
             sidebar.classList.toggle("sidebar--active");
+            overlay.classList.toggle("overlay--active");
         })
+    })
+
+    // Smooth scroll handler
+    const scrollButton = document.getElementById("scroll-button");
+    const contentSection = document.getElementById("about");
+
+    scrollButton.addEventListener("click", () => {
+        contentSection.scrollIntoView({ behavior: "smooth" });
+    });
+
+    // "Become partner" button handler
+    const becomePartnerBtn = document.getElementById("become-partner");
+    const partnerPopup = document.querySelector(".partner-popup");
+    const closePartnerPopupBtn = document.getElementById("close-partner-popup");
+
+    becomePartnerBtn.addEventListener("click", () => {
+        overlay.classList.add("overlay--active");
+        partnerPopup.classList.add("partner-popup--active");
+    })
+
+    closePartnerPopupBtn.addEventListener("click", () => {
+        overlay.classList.remove("overlay--active");
+        partnerPopup.classList.remove("partner-popup--active");
     })
 })
